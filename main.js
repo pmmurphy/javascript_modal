b2bde715acb4b51062a5079736b2690f501de6e3
Paula Murphy <paulam849@gmail.com>
2015-12-17 19:19:32 -0500

    changed css

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-main.js
+$(".lightBoxLink").on("click", function(){
+	$("#image1").attr("src",
+		$(this).attr("src"));
+	$(".outerLightBox").fadeIn
+	(2500);
+})
+
+if ($(this).attr("src")== "lion.jpg"){
+	$("#text1").text("You looking at me");
+	$("img").show();
+}
+
+else if ($(this).attr("src") == "Zebra.jpg"){
+	$("img").show();
+	$("#text1").text("I have stripes");
+	
+} 
+
+else{
+	$("img").hide();
+	$("#text1").text("Sorry no images");
+	$("#text1").css({
+		"margin":"300px"
+	});
+
+};
+
+$(".outerLightBox").on("click",function(e){
+	e.preventDefault();
+	$(".outerLightBox").hide();
+	$("#text1").css("margin", "0px");
+});
+
+
